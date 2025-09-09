66c804020146614b63df06e66aeeb736c87299fd
yoga1989 <104570611+yoga1989@users.noreply.github.com>
2025-09-09 06:10:21 +0000

    commiting pull req code

---
+++ pullReqAlert.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "https://api.github.com/repos/yoga1989/pythoncodes/pulls"
+response = requests.get(url).json()
+#print(response)
+if response:
+    for i in range(len(response)):
+        print(response[i]["user"]["login"] + " has 1 pull request to be approved")
+else:
+    print("No Pull request to approve")
